252a1bea8564736c45fd5d7c5f8f13c14e0241e6
web3yurii <web3yurii@gmail.com>
2025-02-05 21:56:17 +0200

    Building your first hook

--- src/PointsHook.sol
+++ src/PointsHook.sol
@@ -20,40 +20,32 @@ contract PointsHook is BaseHook, ERC20 {
     using BalanceDeltaLibrary for BalanceDelta;
 
     // Initialize BaseHook and ERC20
-    constructor(
-        IPoolManager _manager,
-        string memory _name,
-        string memory _symbol
-    ) BaseHook(_manager) ERC20(_name, _symbol, 18) {}
+    constructor(IPoolManager _manager, string memory _name, string memory _symbol)
+        BaseHook(_manager)
+        ERC20(_name, _symbol, 18)
+    {}
 
     // Set up hook permissions to return `true`
     // for the two hook functions we are using
-    function getHookPermissions()
-        public
-        pure
-        override
-        returns (Hooks.Permissions memory)
-    {
-        return
-            Hooks.Permissions({
-                beforeInitialize: false,
-                afterInitialize: false,
-                beforeAddLiquidity: false,
-                beforeRemoveLiquidity: false,
-                afterAddLiquidity: true,
-                afterRemoveLiquidity: false,
-                beforeSwap: false,
-                afterSwap: true,
-                beforeDonate: false,
-                afterDonate: false,
-                beforeSwapReturnDelta: false,
-                afterSwapReturnDelta: false,
-                afterAddLiquidityReturnDelta: false,
-                afterRemoveLiquidityReturnDelta: false
-            });
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterAddLiquidity: true,
+            afterRemoveLiquidity: false,
+            beforeSwap: false,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
     }
 
-    // Stub implementation of `afterSwap`
     function afterSwap(
         address,
         PoolKey calldata key,
@@ -61,11 +53,30 @@ contract PointsHook is BaseHook, ERC20 {
         BalanceDelta delta,
         bytes calldata hookData
     ) external override onlyPoolManager returns (bytes4, int128) {
-        // We'll add more code here shortly
+        // If this is not an ETH-TOKEN pool with this hook attached, ignore
+        if (!key.currency0.isAddressZero()) return (this.afterSwap.selector, 0);
+
+        // We only mint points if user is buying TOKEN with ETH
+        if (!swapParams.zeroForOne) return (this.afterSwap.selector, 0);
+
+        // Mint points equal to 20% of the amount of ETH they spent
+        // Since its a zeroForOne swap:
+        // if amountSpecified < 0:
+        //      this is an "exact input for output" swap
+        //      amount of ETH they spent is equal to |amountSpecified|
+        // if amountSpecified > 0:
+        //      this is an "exact output for input" swap
+        //      amount of ETH they spent is equal to BalanceDelta.amount0()
+
+        uint256 ethSpendAmount = uint256(int256(-delta.amount0()));
+        uint256 pointsForSwap = ethSpendAmount / 5;
+
+        // Mint the points
+        _assignPoints(hookData, pointsForSwap);
+
         return (this.afterSwap.selector, 0);
     }
 
-    // Stub implementation for `afterAddLiquidity`
     function afterAddLiquidity(
         address,
         PoolKey calldata key,
@@ -74,7 +85,15 @@ contract PointsHook is BaseHook, ERC20 {
         BalanceDelta,
         bytes calldata hookData
     ) external override onlyPoolManager returns (bytes4, BalanceDelta) {
-        // We'll add more code here shortly
+        // If this is not an ETH-TOKEN pool with this hook attached, ignore
+        if (!key.currency0.isAddressZero()) return (this.afterSwap.selector, delta);
+
+        // Mint points equivalent to how much ETH they're adding in liquidity
+        uint256 pointsForAddingLiquidity = uint256(int256(-delta.amount0()));
+
+        // Mint the points
+        _assignPoints(hookData, pointsForAddingLiquidity);
+
         return (this.afterAddLiquidity.selector, delta);
     }
 
