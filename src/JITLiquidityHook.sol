6001f97982c9965e5e581d3722f0c0bdfdefd0a0
web3yurii <web3yurii@gmail.com>
2025-02-16 20:22:32 +0200

    add just in time liquidity hook

---
+++ src/JITLiquidityHook.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
+
+contract JITLiquidityHook is BaseHook {
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {
+        updateMovingAverage();
+    }
+}
